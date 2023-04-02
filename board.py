c730f786c32917d67d94dd7b60711a42efd7da70
Elliott Morgan <awesomescar16@outlook.com>
2023-04-02 19:36:06 +0100

    Adjusted to work with pygame zero.

--- board.py
+++ board.py
@@ -1,18 +1,28 @@
-class Board():
-    """
-    The main set up of the chess board.
-    """
-    def __init__(self):
-                self.board = [
-            ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
-            ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-            ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-            ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
-        ]
+import pgzrun
+TILE_SIZE = 60
+WIDTH = 480
+HEIGHT = 480
+background = Actor("background")
+board = [
+    ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br'],
+    ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+    ['__', '__', '__', '__', '__', '__', '__', '__'],
+    ['__', '__', '__', '__', '__', '__', '__', '__'],
+    ['__', '__', '__', '__', '__', '__', '__', '__'],
+    ['__', '__', '__', '__', '__', '__', '__', '__'],
+    ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+    ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr']
+    ]
+def draw():
+    screen.clear()
+    background.draw()
+    for row in range(len(board)):
+        for column in range(len(board[row])):
+            x = column * TILE_SIZE
+            y = row * TILE_SIZE
+            tile = board[row][column]
+            screen.blit(tile, (x, y))
+def update():
+    pass
 
-    def print_board(self):
-        pass
+pgzrun.go()
