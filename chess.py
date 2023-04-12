ad23ccaf839352772b12c04f6afb84ab71f33da1
Elliott Morgan <awesomescar16@outlook.com>
2023-04-12 22:14:32 +0100

    Completed game and added extra mode.

--- chess.py
+++ chess.py
@@ -22,18 +22,19 @@ board = [
     ['__', '__', '__', '__', '__', '__', '__', '__'],
     ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
     ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr']
-    ]
+]
 for row in range(len(board)):
     for column in range(len(board[row])):
         x = (column * TILE_SIZE)+30
         y = (row * TILE_SIZE)+30
         tile = board[row][column]
-        piece = Actor((board[row][column]),(x,y))
+        piece = Actor((board[row][column]), (x, y))
         pieces.append(piece)
 
-def move(piece,to):
+
+def move(piece, to):
     global go, last_move
-    check_en_passant(piece,to)
+    check_en_passant(piece, to)
     if go == "w":
         go = "b"
     else:
@@ -49,13 +50,13 @@ def move(piece,to):
             board[x][y] = "__"
     for i in pieces:
         if i.pos == to:
-            #Change piece if promoted
-            i.image = check_promotion(to,img)
+            # Change piece if promoted
+            i.image = check_promotion(to, img)
             position = find_piece(i)
             x, y = position
             x = int(x)
             y = int(y)
-            #Check if king is captured
+            # Check if king is captured
             if board[x][y] == "wk":
                 print("Black wins.")
                 exit()
@@ -63,8 +64,8 @@ def move(piece,to):
                 print("White wins.")
                 exit()
             board[x][y] = img
-            #Move rook if castling
-            castle_check(to,img)
+            # Move rook if castling
+            castle_check(to, img)
     # Check if the piece is a pawn that just moved two squares
     print(piece.pos)
     print(to)
@@ -76,71 +77,74 @@ def move(piece,to):
     else:
         last_move = None
 
-def check_en_passant(piece,to):
+
+def check_en_passant(piece, to):
     global last_move
-    #Check en passant for white
-    if piece.image == "wp" and to == (last_move,150):
+    # Check en passant for white
+    if piece.image == "wp" and to == (last_move, 150):
         board[4][int((last_move-30)/60)] = "__"
         for i in pieces:
-            if i.pos == (last_move,210):
+            if i.pos == (last_move, 210):
                 i.image = "__"
-    #Check en passant for black
-    if piece.image == "bp" and to == (last_move,330):
+    # Check en passant for black
+    if piece.image == "bp" and to == (last_move, 330):
         board[4][int((last_move-30)/60)] = "__"
         for i in pieces:
-            if i.pos == (last_move,270):
+            if i.pos == (last_move, 270):
                 i.image = "__"
 
 
-def check_promotion(to,img):
+def check_promotion(to, img):
     colour = img[0]
     promotes = {
-        "Queen" : colour + "q",
-        "Rook"  : colour + "r",
+        "Queen": colour + "q",
+        "Rook": colour + "r",
         "Knight": colour + "n",
         "Bishop": colour + "b"
-        }
+    }
     if to[1] == 30.0 and img == "wp" or to[1] == 450.0 and img == "bp":
-            promote = 0
-            while promote not in promotes:
-                promote = input("What would you like to promote to: ")
-            return promotes[promote]
-    else: 
+        promote = 0
+        while promote not in promotes:
+            promote = input("What would you like to promote to: ")
+        return promotes[promote]
+    else:
         return img
 
-def castle_check(to,img):
+
+def castle_check(to, img):
     global white_castle, black_castle
-    #White castle
+    # White castle
     if img == "wk" and white_castle:
         white_castle = False
-        #Short castle
+        # Short castle
         if to == (390.0, 450.0):
             pieces[63].image = "__"
             pieces[61].image = "wr"
-            board[7][7]="__"
-            board[7][5]="wr"
-        #Long castle
+            board[7][7] = "__"
+            board[7][5] = "wr"
+        # Long castle
         if img == "wk" and to == (150.0, 450.0) and white_castle:
             pieces[56].image = "__"
             pieces[59].image = "wr"
-            board[7][0]="__"
-            board[7][3]="wr"
-    #Black castle
+            board[7][0] = "__"
+            board[7][3] = "wr"
+    # Black castle
     elif img == "bk" and black_castle:
         black_castle = False
-        #Short castle
+        # Short castle
         if to == (390.0, 30.0):
             pieces[7].image = "__"
             pieces[5].image = "br"
-            board[0][7]="__"
-            board[0][5]="br"
-        #Long castle
+            board[0][7] = "__"
+            board[0][5] = "br"
+        # Long castle
         if img == "bk" and to == (150.0, 30.0) and black_castle:
             pieces[0].image = "__"
             pieces[3].image = "br"
-            board[0][0]="__"
-            board[0][3]="br"
-                
+            board[0][0] = "__"
+            board[0][3] = "br"
+
+
 def draw():
     screen.clear()
     background.draw()
@@ -151,6 +155,7 @@ def draw():
         piece.draw()
     for squares in valid_moves:
         squares.draw()
+
 
 def on_mouse_down(pos, button):
     global selected, go, valid_moves, takeable
@@ -184,6 +189,7 @@ def find_piece(piece):
     y = int((piece.x - 30) / 60)
     return (x, y)
 
+
 def check_valid(piece):
     piece_types = {
         'wr': check_rook_moves,
@@ -205,13 +211,15 @@ def check_valid(piece):
     else:
         op_colour = 'b'
     if piece_type in piece_types:
-        piece_types[piece_type](piece,op_colour)
+        piece_types[piece_type](piece, op_colour)
 
-def check_queen_moves(piece,op_colour):
-    check_rook_moves(piece,op_colour)
-    check_bishop_moves(piece,op_colour)
 
-def check_rook_moves(piece,op_colour):
+def check_queen_moves(piece, op_colour):
+    check_rook_moves(piece, op_colour)
+    check_bishop_moves(piece, op_colour)
+
+
+def check_rook_moves(piece, op_colour):
     position = find_piece(piece)
     x, y = position
     x = int(x)
@@ -220,9 +228,9 @@ def check_rook_moves(piece,op_colour):
     for i in range(y - 1, -1, -1):
         current_square = board[x][i]
         if current_square == "__":
-            valid_moves.append(Actor(("moves"),(i*60+30,x*60+30)))
+            valid_moves.append(Actor(("moves"), (i*60+30, x*60+30)))
         elif current_square[0] == op_colour:
-            takeable.append(Actor(("take"),(i*60+30,x*60+30)))
+            takeable.append(Actor(("take"), (i*60+30, x*60+30)))
             break
         else:
             break
@@ -230,9 +238,9 @@ def check_rook_moves(piece,op_colour):
     for i in range(y + 1, 8):
         current_square = board[x][i]
         if current_square == "__":
-            valid_moves.append(Actor(("moves"),(i*60+30,x*60+30)))
+            valid_moves.append(Actor(("moves"), (i*60+30, x*60+30)))
         elif current_square[0] == op_colour:
-            takeable.append(Actor(("take"),(i*60+30,x*60+30)))
+            takeable.append(Actor(("take"), (i*60+30, x*60+30)))
             break
         else:
             break
@@ -240,9 +248,9 @@ def check_rook_moves(piece,op_colour):
     for i in range(x - 1, -1, -1):
         current_square = board[i][y]
         if current_square == "__":
-            valid_moves.append(Actor(("moves"),(y*60+30,i*60+30)))
+            valid_moves.append(Actor(("moves"), (y*60+30, i*60+30)))
         elif current_square[0] == op_colour:
-            takeable.append(Actor(("take"),(y*60+30,i*60+30)))
+            takeable.append(Actor(("take"), (y*60+30, i*60+30)))
             break
         else:
             break
@@ -250,12 +258,13 @@ def check_rook_moves(piece,op_colour):
     for i in range(x + 1, 8):
         current_square = board[i][y]
         if current_square == "__":
-            valid_moves.append(Actor(("moves"),(y*60+30,i*60+30)))
+            valid_moves.append(Actor(("moves"), (y*60+30, i*60+30)))
         elif current_square[0] == op_colour:
-            takeable.append(Actor(("take"),(y*60+30,i*60+30)))
+            takeable.append(Actor(("take"), (y*60+30, i*60+30)))
             break
         else:
             break
+
 
 def check_bishop_moves(piece, op_colour):
     position = find_piece(piece)
@@ -266,9 +275,9 @@ def check_bishop_moves(piece, op_colour):
     for i, j in zip(range(x-1, -1, -1), range(y-1, -1, -1)):
         current_square = board[i][j]
         if current_square == "__":
-            valid_moves.append(Actor(("moves"),(j*60+30,i*60+30)))
+            valid_moves.append(Actor(("moves"), (j*60+30, i*60+30)))
         elif current_square[0] == op_colour:
-            takeable.append(Actor(("take"),(j*60+30,i*60+30)))
+            takeable.append(Actor(("take"), (j*60+30, i*60+30)))
             break
         else:
             break
@@ -276,9 +285,9 @@ def check_bishop_moves(piece, op_colour):
     for i, j in zip(range(x-1, -1, -1), range(y+1, 8)):
         current_square = board[i][j]
         if current_square == "__":
-            valid_moves.append(Actor(("moves"),(j*60+30,i*60+30)))
+            valid_moves.append(Actor(("moves"), (j*60+30, i*60+30)))
         elif current_square[0] == op_colour:
-            takeable.append(Actor(("take"),(j*60+30,i*60+30)))
+            takeable.append(Actor(("take"), (j*60+30, i*60+30)))
             break
         else:
             break
@@ -286,9 +295,9 @@ def check_bishop_moves(piece, op_colour):
     for i, j in zip(range(x+1, 8), range(y-1, -1, -1)):
         current_square = board[i][j]
         if current_square == "__":
-            valid_moves.append(Actor(("moves"),(j*60+30,i*60+30)))
+            valid_moves.append(Actor(("moves"), (j*60+30, i*60+30)))
         elif current_square[0] == op_colour:
-            takeable.append(Actor(("take"),(j*60+30,i*60+30)))
+            takeable.append(Actor(("take"), (j*60+30, i*60+30)))
             break
         else:
             break
@@ -296,102 +305,106 @@ def check_bishop_moves(piece, op_colour):
     for i, j in zip(range(x+1, 8), range(y+1, 8)):
         current_square = board[i][j]
         if current_square == "__":
-            valid_moves.append(Actor(("moves"),(j*60+30,i*60+30)))
+            valid_moves.append(Actor(("moves"), (j*60+30, i*60+30)))
         elif current_square[0] == op_colour:
-            takeable.append(Actor(("take"),(j*60+30,i*60+30)))
+            takeable.append(Actor(("take"), (j*60+30, i*60+30)))
             break
         else:
             break
 
-def check_pawn_moves(piece,op_colour):
+
+def check_pawn_moves(piece, op_colour):
     global last_move
     position = find_piece(piece)
     x, y = position
     x = int(x)
     y = int(y)
-    #Check moves for white pawn
+    # Check moves for white pawn
     if op_colour == "b":
         if board[x-1][y] == "__":
-            valid_moves.append(Actor(("moves"),(y*60+30,(x-1)*60+30)))
+            valid_moves.append(Actor(("moves"), (y*60+30, (x-1)*60+30)))
         if y > 0:
-            if board[x-1][y-1][0]=="b":
-                takeable.append(Actor(("take"),((y-1)*60+30,(x-1)*60+30))) 
+            if board[x-1][y-1][0] == "b":
+                takeable.append(Actor(("take"), ((y-1)*60+30, (x-1)*60+30)))
         if y < 7:
-            if board[x-1][y+1][0]=="b":
-                takeable.append(Actor(("take"),((y+1)*60+30,(x-1)*60+30))) 
+            if board[x-1][y+1][0] == "b":
+                takeable.append(Actor(("take"), ((y+1)*60+30, (x-1)*60+30)))
         if x == 6 and board[x-2][y] == "__" and board[x-1][y] == "__":
-            valid_moves.append(Actor(("moves"),(y*60+30,(x-2)*60+30)))
+            valid_moves.append(Actor(("moves"), (y*60+30, (x-2)*60+30)))
         if x == 3 and (y*60+90 == last_move or y*60-30 == last_move):
-            takeable.append(Actor(("take"),(last_move,150))) 
-    #Check moves for black pawn
+            takeable.append(Actor(("take"), (last_move, 150)))
+    # Check moves for black pawn
     else:
         if board[x+1][y] == "__":
-            valid_moves.append(Actor(("moves"),(y*60+30,(x+1)*60+30)))
+            valid_moves.append(Actor(("moves"), (y*60+30, (x+1)*60+30)))
         if y > 0:
-            if board[x+1][y-1][0]=="w":
-                takeable.append(Actor(("take"),((y-1)*60+30,(x+1)*60+30))) 
+            if board[x+1][y-1][0] == "w":
+                takeable.append(Actor(("take"), ((y-1)*60+30, (x+1)*60+30)))
         if y < 7:
-            if board[x+1][y+1][0]=="w":
-                takeable.append(Actor(("take"),((y+1)*60+30,(x+1)*60+30))) 
+            if board[x+1][y+1][0] == "w":
+                takeable.append(Actor(("take"), ((y+1)*60+30, (x+1)*60+30)))
         if x == 1 and board[x+2][y] == "__" and board[x+1][y] == "__":
-            valid_moves.append(Actor(("moves"),(y*60+30,(x+2)*60+30)))
+            valid_moves.append(Actor(("moves"), (y*60+30, (x+2)*60+30)))
         if x == 4 and (y*60+90 == last_move or y*60-30 == last_move):
-            takeable.append(Actor(("take"),(last_move,330))) 
+            takeable.append(Actor(("take"), (last_move, 330)))
 
 
-def check_knight_moves(piece,op_colour):
+def check_knight_moves(piece, op_colour):
     position = find_piece(piece)
     x, y = position
     x = int(x)
     y = int(y)
-    #The different offsets for the knight movement
+    # The different offsets for the knight movement
     knight_moves = [
-        [ 2, 1],
-        [ 2,-1],
-        [ 1,-2],
-        [ 1, 2],
+        [2, 1],
+        [2, -1],
+        [1, -2],
+        [1, 2],
         [-1, 2],
-        [-1,-2],
+        [-1, -2],
         [-2, 1],
-        [-2,-1]]
+        [-2, -1]]
     for i in range(len(knight_moves)):
         new_x = x + knight_moves[i][1]
         new_y = y + knight_moves[i][0]
-        if new_x<0 or new_x>7 or new_y<0 or new_y>7:
+        if new_x < 0 or new_x > 7 or new_y < 0 or new_y > 7:
             continue
         if board[new_x][new_y][0] == op_colour:
-            takeable.append(Actor(("take"),(new_y*60+30,new_x*60+30))) 
+            takeable.append(Actor(("take"), (new_y*60+30, new_x*60+30)))
         elif board[new_x][new_y] == "__":
-            valid_moves.append(Actor(("moves"),(new_y*60+30,new_x*60+30)))
+            valid_moves.append(Actor(("moves"), (new_y*60+30, new_x*60+30)))
 
-def check_king_moves(piece,op_colour):
+
+def check_king_moves(piece, op_colour):
     global white_castle, black_castle
     position = find_piece(piece)
     x, y = position
     x = int(x)
     y = int(y)
-    #Check squares around king
-    for i in range(-1,2):
-        for j in range(-1,2):
+    # Check squares around king
+    for i in range(-1, 2):
+        for j in range(-1, 2):
             new_x = x + i
             new_y = y + j
-            if new_x<0 or new_x>7 or new_y<0 or new_y>7 or(i==0 and j==0):
+            if new_x < 0 or new_x > 7 or new_y < 0 or new_y > 7 or (i == 0 and j == 0):
                 continue
             if board[new_x][new_y][0] == op_colour:
-                takeable.append(Actor(("take"),(new_y*60+30,new_x*60+30))) 
+                takeable.append(Actor(("take"), (new_y*60+30, new_x*60+30)))
             elif board[new_x][new_y] == "__":
-                valid_moves.append(Actor(("moves"),(new_y*60+30,new_x*60+30)))
-    #Check short castling for white
-    if op_colour == "b" and white_castle and board[x][y+1] == "__" and board[x][y+2]=="__" and board[x][y+3] == "wr":
-        valid_moves.append(Actor(("moves"),((y+2)*60+30,x*60+30)))
-    #Check long castling for white
-    if op_colour == "b" and white_castle and board[x][y-1] == "__" and board[x][y-2]=="__" and board[x][y-3] == "__" and board[x][y-4] == "wr":
-        valid_moves.append(Actor(("moves"),((y-2)*60+30,x*60+30)))
-    #Check short castling for black
-    if op_colour == "w" and black_castle and board[x][y+1] == "__" and board[x][y+2]=="__" and board[x][y+3] == "br":
-        valid_moves.append(Actor(("moves"),((y+2)*60+30,x*60+30)))
-    #Check long castling for black
-    if op_colour == "w" and black_castle and board[x][y-1] == "__" and board[x][y-2]=="__" and board[x][y-3] == "__" and board[x][y-4] == "br":
-        valid_moves.append(Actor(("moves"),((y-2)*60+30,x*60+30)))
-    
+                valid_moves.append(
+                    Actor(("moves"), (new_y*60+30, new_x*60+30)))
+    # Check short castling for white
+    if op_colour == "b" and white_castle and board[x][y+1] == "__" and board[x][y+2] == "__" and board[x][y+3] == "wr":
+        valid_moves.append(Actor(("moves"), ((y+2)*60+30, x*60+30)))
+    # Check long castling for white
+    if op_colour == "b" and white_castle and board[x][y-1] == "__" and board[x][y-2] == "__" and board[x][y-3] == "__" and board[x][y-4] == "wr":
+        valid_moves.append(Actor(("moves"), ((y-2)*60+30, x*60+30)))
+    # Check short castling for black
+    if op_colour == "w" and black_castle and board[x][y+1] == "__" and board[x][y+2] == "__" and board[x][y+3] == "br":
+        valid_moves.append(Actor(("moves"), ((y+2)*60+30, x*60+30)))
+    # Check long castling for black
+    if op_colour == "w" and black_castle and board[x][y-1] == "__" and board[x][y-2] == "__" and board[x][y-3] == "__" and board[x][y-4] == "br":
+        valid_moves.append(Actor(("moves"), ((y-2)*60+30, x*60+30)))
+
+
 pgzrun.go()
